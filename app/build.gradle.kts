b1219298788229de6db9feb909590311226cb314
huojieren <1956934061@qq.com>
2025-01-19 16:46:41 +0800

    feat: 添加权限请求、悬浮窗和应用使用时长监控功能
    - 更新 AndroidManifest.xml，添加必要的权限声明
    - 修改 build.gradle.kts 和 libs.versions.toml，添加依赖和配置
    - 新增 MainActivity 和 activity_main.xml，实现悬浮窗、通知和使用情况访问权限的请求
    - 创建悬浮窗功能，包括 WindowManager 初始化、悬浮窗视图设置和悬浮窗参数配置
    - 添加时间选择器和确认按钮，允许用户选择使用时长并保存
    - 在主界面添加悬浮窗显示测试按钮，并新增 floating_window.xml 布局文件
    - 添加开始监控按钮和应用使用时长监控功能，优化界面布局和按钮文本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,14 +33,18 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    viewBinding {
+        enable = true
+    }
 }
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.constraintlayout)
 }
