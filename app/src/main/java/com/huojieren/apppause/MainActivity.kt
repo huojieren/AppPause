617c0c7c6737ccd8d291349168d1ac385c1b76e3
huojieren <1956934061@qq.com>
2026-04-08 15:11:38 +0800

    bugfix: 修复亮色模式下状态栏文字和图标不可见的问题
    - 添加了 Android 11(R) 及以上版本的系统栏外观控制功能
    - 实现边缘到边缘显示和系统栏外观控制，浅色/深色主题下的系统栏颜色适配

--- app/src/main/java/com/huojieren/apppause/MainActivity.kt
+++ app/src/main/java/com/huojieren/apppause/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.huojieren.apppause
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.SideEffect
 import com.huojieren.apppause.ui.AppPauseApp
 import com.huojieren.apppause.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        
         setContent {
+            val isDarkTheme = isSystemInDarkTheme()
+
+            SideEffect {
+                window.insetsController?.setSystemBarsAppearance(
+                    if (isDarkTheme) 0 else WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                )
+            }
+            
             AppTheme {
                 AppPauseApp()
             }
